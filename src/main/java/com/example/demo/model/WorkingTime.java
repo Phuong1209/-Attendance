b12849ea5e1be86ad84677bd648cc5711430886c
HuongKumanthong <maihuong19j1@gmail.com>
2024-10-21 15:38:58 +0700

    add workingtime

---
+++ src/main/java/com/example/demo/model/WorkingTime.java
@@ -0,0 +1,31 @@
+package com.example.demo.model;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "WorkingTime")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class WorkingTime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String user_id;
+    private LocalDate date;
+    private LocalDateTime checkin_time;
+    private LocalDateTime checkout_time;
+    private LocalDateTime breaktime;
+    private LocalDateTime worktime;
+    private LocalDateTime overtime;
+    private LocalDate create_at;
+    private LocalDateTime update_at;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User users;
+}
+
+
